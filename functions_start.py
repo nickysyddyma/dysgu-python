2c8835fa1179f8a326475e32dac61c8bced92b48
Nicky Roberts <57113402+nickysyddyma@users.noreply.github.com>
2024-09-05 20:35:44 +0100

    Add files via upload

---
+++ functions_start.py
@@ -0,0 +1,25 @@
+#
+# Example file for working with functions
+# LinkedIn Learning Python course by Joe Marini
+#
+
+
+# TODO: define a basic function
+def HelloWorld():
+    print("Hello World")
+
+
+# TODO: function that takes arguments
+
+
+# TODO: function that returns a value
+
+
+# TODO: function with default value for an argument
+
+
+# TODO: function with variable number of arguments
+
+HelloWorld()
+
+
